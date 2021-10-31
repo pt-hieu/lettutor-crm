55e10bcfd2aae4e7c38d72ac26ac48c2bd90ab05
Hieu Pham <hieupham@kms-technology.com>
2021-10-31 21:22:42 +0700

    fix: validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,14 @@ import 'dotenv/config.js'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from '@/app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }))
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
