708dd06cc04a29b231166689dcc6b89fb750559e
Hieu Pham <hieupham@kms-technology.com>
2021-11-01 22:40:19 +0700

    feat: swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,20 +2,29 @@ import 'dotenv/config.js'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from '@/app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  )
   app.setGlobalPrefix('api')
   app.use(cookieParser())
   app.enableCors({
     credentials: true,
     origin: [process.env.FE_URL],
   })
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder().setVersion('0.0.1').build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('docs', app, document)
+  }
 
   await app.listen(8000)
 }
