b482a73224890ca06925d0e50188926b8a6b5e77
Hieu Pham <hieupham@kms-technology.com>
2021-11-07 16:44:31 +0700

    fix: script

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker build . -t crm:backend
-docker stop crm:backend || true
-docker rm crm:backend || true
-docker run --rm --name crm:backend -d -p 3000:3000 crm:backend
+docker stop backend || true
+docker rm backend || true
+docker run --rm --name backend -d -p 3000:3000 crm:backend
