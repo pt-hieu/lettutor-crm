720f6a9e689e9aab69da75aebebf6a1ac7c69b14
Hieu Pham <hieupham@kms-technology.com>
2021-11-16 21:53:16 +0700

    feat: access token for testing

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -3,13 +3,11 @@ import { Public } from 'src/utils/decorators/public.decorator'
 import {
   Body,
   Controller,
-  Delete,
   Post,
-  Request,
-  Response,
+  Response as Res,
 } from '@nestjs/common'
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
-import { Response as ExpressResponse, Request as ExpressRequest } from 'express'
+import { Response } from 'express'
 import { AuthService } from './auth.service'
 
 @ApiTags('auth')
@@ -19,14 +17,17 @@ export class AuthController {
   constructor(private readonly service: AuthService) { }
 
   @Public()
-  @ApiOperation({ summary: "to validate credentials" })
+  @ApiOperation({ summary: 'to validate credentials' })
   @Post('validate')
-  logIn(@Body() body: DTO.Auth.Login) {
-    return this.service.validate(body)
+  logIn(
+    @Body() body: DTO.Auth.Login,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.service.validate(body, res)
   }
 
   @Public()
-  @ApiOperation({ summary: "to signup" })
+  @ApiOperation({ summary: 'to signup' })
   @Post('signup')
   signUp(@Body() body: DTO.Auth.SignUp) {
     return this.service.signup(body)
