fb9a8728a2466fc79d9fabcaf584249ce3131f92
MinHie <55249566+leminhhieust@users.noreply.github.com>
2021-12-01 20:43:44 +0700

    feat: view lead detail

--- backend/src/lead-contact/lead-contact.service.ts
+++ backend/src/lead-contact/lead-contact.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { DTO } from 'src/type'
+import { JwtPayload } from 'src/utils/interface'
 import { Repository } from 'typeorm'
 import { LeadContact } from './lead-contact.entity'
 
@@ -10,6 +11,10 @@ export class LeadContactService {
     @InjectRepository(LeadContact)
     private leadContactRepo: Repository<LeadContact>,
   ) {}
+
+  async getLeadById(id: string) {
+    return this.leadContactRepo.findOne({ id })
+  }
 
   async addLead(dto: DTO.LeadContact.AddLead) {
     return this.leadContactRepo.save(dto)
