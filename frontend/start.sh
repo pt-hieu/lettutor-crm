e104069a7ebf918826e386c47d81880dc6321a45
Pham Trung Hieu <trunghieu.dev.2711@gmail.com>
2021-12-02 09:46:15 +0700

    fix: script

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker build . -t crm:frontend
 docker stop frontend || true
-docker rm fronend || true
+docker build . -t crm:frontend
 docker run --rm --name frontend -d -p 3000:3000 crm:frontend
