24e7d8ee1fe3af6741753cd3ea8dba59396d2503
Pham Trung Hieu <trunghieu.dev.2711@gmail.com>
2021-12-02 09:46:52 +0700

    fix: script

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-docker build . -t crm:backend
 docker stop backend || true
-docker rm backend || true
+docker build . -t crm:backend
 docker run --rm --name backend -d -p 8000:8000 crm:backend
