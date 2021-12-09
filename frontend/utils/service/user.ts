5e87cdb3491bf03b8fd7db213ab1d0c35a89c25a
Hieu Pham <hieupham@kms-technology.com>
2021-12-09 15:45:43 +0700

    fix: build

--- frontend/utils/service/user.ts
+++ frontend/utils/service/user.ts
@@ -18,7 +18,7 @@ export const changePassword = async (data: {
 
 export const getSelf = (token?: string) => () =>
   axios
-    .get<Pick<User, 'name' | 'email' | 'status'>>(API + '/api/user/self', {
+    .get<Pick<User, 'name' | 'email' | 'status' | 'roles'>>(API + '/api/user/self', {
       headers: { authorization: 'Bearer ' + token },
     })
     .then((res) => res.data)
