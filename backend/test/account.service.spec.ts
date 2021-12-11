e801bf437d88c4b28a79b23e3c456970a32415af
nhlong20 <hoanglongmcs1@gmail.com>
2021-12-11 10:19:38 +0700

    test: search for  accounts

--- backend/test/account.service.spec.ts
+++ backend/test/account.service.spec.ts
@@ -2,9 +2,8 @@ import { DTO } from 'src/type'
 import { Test, TestingModule } from '@nestjs/testing'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
-import { account } from './data'
+import { account, lead } from './data'
 import { mockQueryBuilder, MockType, repositoryMockFactory } from './utils'
-import { LeadContact } from 'src/lead-contact/lead-contact.entity'
 import { IPaginationMeta, Pagination } from 'nestjs-typeorm-paginate'
 import { AccountService } from 'src/account/account.service'
 import { Account } from 'src/account/account.entity'
@@ -33,6 +32,22 @@ describe('lead-contact service', () => {
       const dto: DTO.Account.GetManyQuery = {
         limit: 10,
         page: 1,
+        shouldNotPaginate: false,
+      }
+
+      mockQueryBuilder.getMany.mockReturnValue([account])
+
+      expect(
+        (
+          (await accountService.getMany(dto)) as Pagination<Account, IPaginationMeta>
+        ).items,
+      ).toEqual([account])
+    })
+    it('should return account that matches the search successfully', async () => {
+      const dto: DTO.Account.GetManyQuery = {
+        limit: 10,
+        page: 1,
+        search: lead.fullName + ' Account',
         shouldNotPaginate: false,
       }
 
