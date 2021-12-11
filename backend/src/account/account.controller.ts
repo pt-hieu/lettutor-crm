e2ec4d8d39688b3823a24043eb7205c0327dfaca
nhlong20 <hoanglongmcs1@gmail.com>
2021-12-11 09:59:08 +0700

    feat: search for account

--- backend/src/account/account.controller.ts
+++ backend/src/account/account.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { DTO } from 'src/type';
+import { AccountService } from './account.service';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private readonly service: AccountService) { }
+
+  @Get()
+  @ApiOperation({ summary: 'view and search all accounts' })
+  @ApiQuery({ type: DTO.Account.GetManyQuery })
+  index(@Query() query: DTO.Account.GetManyQuery) {
+    return this.service.getMany(query)
+  }
+
+}
--- backend/src/account/account.service.ts
+++ backend/src/account/account.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { paginate } from 'nestjs-typeorm-paginate'
 import { DTO } from 'src/type'
-import { Repository } from 'typeorm'
+import { Brackets, Repository } from 'typeorm'
 import { Account } from './account.entity'
 
 @Injectable()
@@ -18,4 +19,18 @@ export class AccountService {
   async getOneById(id: string) {
     return this.accountRepo.findOne({id})
   }
+  async getMany(query: DTO.Account.GetManyQuery) {
+    let q = this.accountRepo
+      .createQueryBuilder('acc')
+      .select()
+      
+    if (query.search) {
+      q = q
+        .andWhere('acc.fullName ILIKE :search', { search: `%${query.search}%` })
+    }
+
+    if (query.shouldNotPaginate === true) return q.getMany()
+    return paginate(q, { limit: query.limit, page: query.page })
+  }
+
 }
