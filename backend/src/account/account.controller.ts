32c8ca27aae879725943d7e165409d1cb836a51f
MinHie <55249566+leminhhieust@users.noreply.github.com>
2021-12-14 20:41:57 +0700

    fix: post account api

--- backend/src/account/account.controller.ts
+++ backend/src/account/account.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { ApiOperation, ApiQuery } from '@nestjs/swagger';
-import { DTO } from 'src/type';
-import { AccountService } from './account.service';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common'
+import { ApiOperation, ApiQuery } from '@nestjs/swagger'
+import { DTO } from 'src/type'
+import { AccountService } from './account.service'
 
 @Controller('account')
 export class AccountController {
-  constructor(private readonly service: AccountService) { }
+  constructor(private readonly service: AccountService) {}
 
   @Get()
   @ApiOperation({ summary: 'view and search all accounts' })
@@ -13,10 +13,10 @@ export class AccountController {
   index(@Query() query: DTO.Account.GetManyQuery) {
     return this.service.getMany(query)
   }
-  
+
   @Post()
   @ApiOperation({ summary: 'to add a new account manually' })
-  addLead(@Body() dto: DTO.Account.AddAccount) {
+  addAccount(@Body() dto: DTO.Account.AddAccount) {
     return this.service.addAccount(dto)
   }
 }
--- backend/src/account/account.module.ts
+++ backend/src/account/account.module.ts
@@ -3,9 +3,10 @@ import { AccountService } from './account.service'
 import { AccountController } from './account.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Account } from './account.entity'
+import { UserModule } from 'src/user/user.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), UserModule],
   providers: [AccountService],
   controllers: [AccountController],
   exports: [AccountService],
