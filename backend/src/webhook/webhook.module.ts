0b8c3cc5c0418999c67c04821c7f4b09bb8d5954
pt-hieu <trunghieu.dev.2711@gmail.com>
2021-12-18 10:45:25 +0700

    fix: build

--- backend/src/webhook/webhook.module.ts
+++ backend/src/webhook/webhook.module.ts
@@ -1,11 +1,17 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
-import { LeadService } from 'src/lead-contact/lead.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { LeadContact } from 'src/lead-contact/lead-contact.entity'
+import { LeadContactModule } from 'src/lead-contact/lead-contact.module'
 import { WebhookController } from '../webhook/webhook.controller'
 import { WebhookService } from './webhook.service'
 
 @Module({
-  imports: [LeadService, HttpModule],
+  imports: [
+    TypeOrmModule.forFeature([LeadContact]),
+    LeadContactModule,
+    HttpModule,
+  ],
   providers: [WebhookService],
   controllers: [WebhookController],
 })
