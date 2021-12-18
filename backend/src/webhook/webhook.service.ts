48b0d75760e730441460339431ca2004bb619b8e
pt-hieu <trunghieu.dev.2711@gmail.com>
2021-12-18 10:32:46 +0700

    refactor: webhook code

--- backend/src/webhook/webhook.service.ts
+++ backend/src/webhook/webhook.service.ts
@@ -2,49 +2,55 @@ import { HttpService } from '@nestjs/axios'
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { lastValueFrom, map } from 'rxjs'
-import { LeadContact, LeadSource, LeadStatus } from 'src/lead-contact/lead-contact.entity'
+import {
+  LeadContact,
+  LeadSource,
+  LeadStatus,
+} from 'src/lead-contact/lead-contact.entity'
 import { LeadService } from 'src/lead-contact/lead.service'
 import { DTO } from 'src/type'
-import { Repository } from 'typeorm'
+
+const ApiVersion = process.env.FACEBOOK_API_VERSION || 'v12.0'
 
 @Injectable()
 export class WebhookService {
   constructor(
     @InjectRepository(LeadContact)
-    private leadContactRepo: Repository<LeadContact>,
     private leadService: LeadService,
-    private httpService: HttpService
-  ) { }
+    private httpService: HttpService,
+  ) {}
 
-  async processNewLeadFromFacebook(leadId: string) {
-    let response;
-    const apiVersion = process.env.FACEBOOK_API_VERSION || "v12.0"
-    let requestURL = `https://graph.facebook.com/${apiVersion}/${leadId}/?access_token=${process.env.FACEBOOK_PAGE_ACCESS_TOKEN}`
-
-    response = await lastValueFrom(
-      this.httpService.get(requestURL).pipe(
-        map((res) => {
-          return res.data;
-        }),
-      ),
-    );
+  async processNewLeadFromFacebook(leadId?: string) {
+    if (!leadId) throw new BadRequestException('Invalid POST data received')
+    
+    const requestURL = `https://graph.facebook.com/${ApiVersion}/${leadId}/?access_token=${process.env.FACEBOOK_PAGE_ACCESS_TOKEN}`
+    const response = await lastValueFrom(
+      this.httpService.get(requestURL).pipe(map((res) => res.data)),
+    )
 
     if (response.error || !response.field_data)
-      throw new BadRequestException(`An invalid response was received from the Facebook API: ${response}`);
+      throw new BadRequestException(
+        `An invalid response was received from the Facebook API: ${response}`,
+      )
 
-    let leadInfo: DTO.Lead.AddLead = new DTO.Lead.AddLead()
-    for (const field of response.field_data) {
-      let fileName:string= field.name
-      fileName = fileName.toLowerCase()
-
-      if(fileName == "email") leadInfo.email = field.values[0]
-      if(fileName == "full_name") leadInfo.fullName = field.values[0]
-      if(fileName == "phone") leadInfo.phoneNum = field.values[0]
-      if(fileName == "street_address") leadInfo.address = field.values[0]
+    const fieldMapping = {
+      email: 'email',
+      full_name: 'fullName',
+      phone: 'phoneNum',
+      street_address: 'address',
     }
+
+    const leadInfo = new DTO.Lead.AddLead()
+
+    ;(response.field_data as { name: string; values: any[] }[]).forEach(
+      (field) => {
+        leadInfo[fieldMapping[field.name.toLocaleLowerCase()]] = field.values[0]
+      },
+    )
+
     leadInfo.status = LeadStatus.NONE
     leadInfo.source = LeadSource.FACEBOOK
-    
+
     return this.leadService.addLead(leadInfo)
   }
 }
