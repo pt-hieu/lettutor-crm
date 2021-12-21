4992f7161b8c2c226f3221e277da1db6d1fce130
pt-hieu <trunghieu.dev.2711@gmail.com>
2021-12-22 00:57:56 +0700

    fix: deployment

--- artemis/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker stop artemis || true
-docker build . -t crm:frontend
-docker run --rm --name artemis -d -p 3000:3000 crm:frontend
---
+++ start.sh
@@ -0,0 +1,8 @@
+#! bin/sh
+
+docker stop artemis || true
+docker pull ghcr.io/pt-hieu/lettutor-crm:artemis
+docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis
+cd ~/lettutor/apollo
+docker-compose down
+docker-compose up -d
