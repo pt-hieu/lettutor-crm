443b90a65d5ef4047341a4577fafa8646a65a2a8
pt-hieu <trunghieu.dev.2711@gmail.com>
2021-12-21 18:17:51 +0000

    fix: typo

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 docker stop artemis || true
 docker pull ghcr.io/pt-hieu/lettutor-crm:artemis
 docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis
-cd ~/lettutor/apollo
+cd ~/lettutor-crm/apollo
 docker-compose down
 docker-compose up -d
