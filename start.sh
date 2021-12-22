8f36bb4ba3fb701a7bdddbc4e61da1a773be8e01
Hieu <trunghieu.dev.2711@gmail.com>
2021-12-22 07:57:31 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ docker pull ghcr.io/pt-hieu/lettutor-crm:artemis
 docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis
 cd ~/lettutor-crm/apollo
 docker-compose down
+docker-compose pull apollo
 docker-compose up -d
