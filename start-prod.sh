5f15b7dddc1c85cd1907ae75558580255981c62b
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-01-23 15:44:15 +0700

    fix: deploy

--- start-prod.sh
+++ start-prod.sh
@@ -4,7 +4,7 @@ docker stop artemis || true
 docker pull ghcr.io/pt-hieu/lettutor-crm:artemis-prod
 docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis-prod
 cd ~/lettutor-crm/apollo
-docker-compose down
-docker-compose pull apollo
-docker-compose up -d
+docker-compose down -f docker-compose.prod.yaml
+docker-compose pull apollo -f docker-compose.prod.yaml
+docker-compose up -d -f docker-compose.prod.yaml
 yes | docker image prune
