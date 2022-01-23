f3f61c40b19461e7b1b5684772ca8da3df18b1e2
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-01-23 17:48:16 +0700

    [skip ci] fix: docker compose

--- start-prod.sh
+++ start-prod.sh
@@ -4,7 +4,7 @@ docker stop artemis || true
 docker pull ghcr.io/pt-hieu/lettutor-crm:artemis-prod
 docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis-prod
 cd ~/lettutor-crm/apollo
-docker-compose down -f docker-compose.prod.yaml
-docker-compose pull apollo -f docker-compose.prod.yaml
-docker-compose up -d -f docker-compose.prod.yaml
+docker-compose -f docker-compose.prod.yaml down 
+docker-compose -f docker-compose.prod.yaml pull apollo
+docker-compose -f docker-compose.prod.yaml up -d 
 yes | docker image prune
