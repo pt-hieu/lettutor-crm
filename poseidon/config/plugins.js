a3e64f252d186f325c36091cb45eb2367af1a64a
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-01-28 02:25:18 +0700

    [skip ci] fix: upload plugin

--- poseidon/config/plugins.js
+++ poseidon/config/plugins.js
@@ -1,14 +1,16 @@
 module.exports = ({ env }) => ({
- upload: {
-   provider: 'aws-s3',
-   providerOptions: {
-     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-     secretAccessKey: env('AWS_ACCESS_SECRET'),
-     region: env('AWS_REGION'),
-     params: {
-       Bucket: env('AWS_BUCKET_NAME'),
-        StorageClass: env('AWS_S3_STORAGE_CLASSES')
-     },
-   }
- }
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
+        params: {
+          Bucket: env("AWS_BUCKET_NAME"),
+          StorageClass: env("AWS_S3_STORAGE_CLASSES"),
+        },
+      },
+    },
+  },
 });
