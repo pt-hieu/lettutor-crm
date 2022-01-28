520a046908bec546111379b7dc47de17b6fb6fd9
Hieu Pham <hieupham@kms-technology.com>
2022-01-28 14:15:11 +0700

    [skip ci] fix: script

--- start.sh
+++ start.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-docker stop artemis || true
-docker pull ghcr.io/pt-hieu/lettutor-crm:artemis
-docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis
-cd ~/lettutor-crm/apollo
-docker-compose down
-docker-compose pull apollo
-docker-compose up -d
+for component in "$@"
+do
+  case ${component} in
+    artemis)
+      docker stop artemis || true
+      docker pull ghcr.io/pt-hieu/lettutor-crm:artemis
+      docker run --rm --name artemis -d -p 3000:3000 ghcr.io/pt-hieu/lettutor-crm:artemis
+      ;;
+    apollo)
+      cd ~/lettutor-crm/apollo
+      docker-compose down
+      docker-compose pull apollo
+      docker-compose up -d
+      ;;
+    poseidon)
+      cd ~/lettutor-crm/poseidon
+      docker-compose down
+      docker-compose pull poseidon
+      docker-compose up -d
+      ;;
+  esac
+done
+
 yes | docker image prune
