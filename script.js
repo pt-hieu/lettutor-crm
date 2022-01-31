76ff42a05211d6adb52b75e56698f8cf931af626
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-01-31 18:43:44 +0700

    refactor: script

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+var _a;
+exports.__esModule = true;
+var yargs = require("yargs");
+var child_process_1 = require("child_process");
+var choices = ["apollo", "artemis", "poseidon"];
+var agrv = yargs.options({
+    block: { type: "array", choices: choices, required: true, "default": choices },
+    init: { type: "boolean", "default": false }
+}).argv;
+var command = "";
+if ("then" in agrv) {
+    yargs.exit(1, new Error("Script failed"));
+}
+else {
+    if (agrv.init) {
+        command =
+            "concurrently " +
+                choices.map(function (choice) { return "\"cd ".concat(choice, " && yarn\""); }).join(" ");
+    }
+    else {
+        command =
+            "concurrently -k " +
+                choices.map(function (choice) { return "\"cd ".concat(choice, " && yarn dev\""); }).join(" ");
+    }
+}
+var concurrentProc = (0, child_process_1.exec)(command, function () { });
+(_a = concurrentProc === null || concurrentProc === void 0 ? void 0 : concurrentProc.stdout) === null || _a === void 0 ? void 0 : _a.pipe(process.stdout);
---
+++ script.ts
@@ -0,0 +1,26 @@
+import * as yargs from "yargs";
+import { exec } from "child_process";
+
+const choices = ["apollo", "artemis", "poseidon"];
+const agrv = yargs.options({
+  block: { type: "array", choices, required: true, default: choices },
+  init: { type: "boolean", default: false },
+}).argv;
+
+let command: string = "";
+if ("then" in agrv) {
+  yargs.exit(1, new Error("Script failed"));
+} else {
+  if (agrv.init) {
+    command =
+      "concurrently " +
+      choices.map((choice) => `"cd ${choice} && yarn"`).join(" ");
+  } else {
+    command =
+      "concurrently -k " +
+      choices.map((choice) => `"cd ${choice} && yarn dev"`).join(" ");
+  }
+}
+
+const concurrentProc = exec(command, () => {});
+concurrentProc?.stdout?.pipe(process.stdout);
