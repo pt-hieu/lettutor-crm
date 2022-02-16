9d13be29337b5aef5f152d32c3e74349101d5c53
pt-hieu <trunghieu.dev.2711@gmail.com>
2022-02-16 12:10:16 +0700

    fix: login

--- artemis/pages/login.tsx
+++ artemis/pages/login.tsx
@@ -1,4 +1,3 @@
-import { notification } from 'antd'
 import Layout from '@utils/components/Layout'
 import { signIn } from 'next-auth/client'
 import { useRouter } from 'next/router'
@@ -7,6 +6,9 @@ import { useForm } from 'react-hook-form'
 import Link from 'next/link'
 import Loading from '@utils/components/Loading'
 import Input from '@utils/components/Input'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { GetServerSideProps } from 'next'
 
 type FormData = {
   email: string
@@ -19,21 +21,42 @@ const errorMapping: Record<string, string> = {
   Callback: 'System failure!',
 }
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .typeError('Email has to be a string')
+    .email('Invalid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .typeError('Password has to be a string')
+    .required('Password is required'),
+})
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {},
+  }
+}
+
 export default function Login() {
   const { query } = useRouter()
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>()
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  })
 
+  const [authError, setAuthError] = useState<string>()
   const [isLoggingIn, setIsLoggingIn] = useState(false)
 
   useEffect(() => {
     if (!('error' in query)) return
-    notification.error({
-      message: errorMapping[query.error as string],
-    })
+    if (query.error === 'undefined') return
+
+    setAuthError(errorMapping[query.error as string])
   }, [query])
 
   const login = useCallback(
@@ -50,21 +73,32 @@ export default function Login() {
   return (
     <Layout footer={false} header={false} title="Login to Artemis">
       <div className="min-h-screen grid w-full place-content-center">
-        <div className="flex items-center">
-          <img
-            src="/illus/login_bg.svg"
-            className="w-[50%] md:hidden"
-            alt="login_background"
-          />
+        <div className="grid grid-cols-[3fr,2fr] max-w-[1200px] border p-8 rounded-lg overflow-hidden shadow-sm">
+          <div className="grid place-content-center bg-blue-600 -my-8 -ml-8 md:hidden">
+            <img
+            // src="/illus/login_bg.svg"
+            // className="block w-1/2 m-auto"
+            // alt="login_background"
+            />
+          </div>
 
           <form
             onSubmit={login}
-            className="min-w-[350px] border border-blue-600 rounded-md px-8 py-16 shadow-md shadow-blue-600/30 mx-auto"
+            className="min-w-[450px] px-8 pl-16 py-16 mx-auto"
           >
-            <div className="mb-16 text-center font-semibold ">
-              <div className="text-blue-600 text-3xl">Welcome to Artemis</div>
-              <div className="text-gray-700">The CRM</div>
+            <div className="mb-8 text-center font-semibold">
+              <div className="text-blue-600 text-3xl">Login</div>
             </div>
+
+            {authError && (
+              <div className="mb-8 text-center text-red-600 px-3 py-2 rounded-md border border-red-600">
+                <button
+                  onClick={() => setAuthError(undefined)}
+                  className="fa fa-times mr-2"
+                ></button>
+                {authError}
+              </div>
+            )}
 
             <div className="mb-4">
               <label htmlFor="email" className="crm-label">
@@ -76,12 +110,7 @@ export default function Login() {
                   id: 'email',
                   type: 'text',
                   className: 'w-full',
-                  ...register('email', {
-                    required: {
-                      value: true,
-                      message: 'Email is required',
-                    },
-                  }),
+                  ...register('email'),
                 }}
               />
             </div>
@@ -97,25 +126,22 @@ export default function Login() {
                   type: 'password',
                   autoComplete: 'currentpassword',
                   className: 'w-full',
-                  ...register('password', {
-                    required: {
-                      value: true,
-                      message: 'Password is required',
-                    },
-                  }),
+                  ...register('password'),
                 }}
               />
             </div>
 
-            <div className="mt-2">
+            <Link href="/reset-password">
+              <a className="inline-block w-full text-right">Forgot password</a>
+            </Link>
+
+            <div className="mt-4">
               <button disabled={isLoggingIn} className="crm-button w-full">
-                <Loading on={isLoggingIn}>Login</Loading>
+                <Loading on={isLoggingIn}>
+                  <span className="fa-solid fa-right-to-bracket mr-2" />
+                  Login
+                </Loading>
               </button>
-              <Link href="/reset-password">
-                <a className="mt-2 inline-block w-full text-right">
-                  Forgot password
-                </a>
-              </Link>
             </div>
           </form>
         </div>
