8903ab1d22b8925d640b06c28a3c0286a941a29c
hieulm2k <leminhhhieust@gmail.com>
2022-03-12 16:03:35 +0700

    refactor: update deal service and dto

--- apollo/src/deal-stage/deal-stage.service.ts
+++ apollo/src/deal-stage/deal-stage.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
+import { FindOneOptions, Repository } from 'typeorm'
+
+import { UtilService } from 'src/global/util.service'
 
 import { DealStage } from './deal-stage.entity'
 
@@ -9,5 +11,21 @@ export class DealStageService {
   constructor(
     @InjectRepository(DealStage)
     private dealStageRepo: Repository<DealStage>,
+    private readonly utilService: UtilService,
   ) {}
+
+  async getDealStageById(option: FindOneOptions<DealStage>, trace?: boolean) {
+    const dealStage = await this.dealStageRepo.findOne(option)
+
+    if (!dealStage) {
+      Logger.error(JSON.stringify(option, null, 2))
+      throw new NotFoundException(`Deal stage not found`)
+    }
+
+    if (trace) {
+      await this.utilService.loadTraceInfo(dealStage)
+    }
+
+    return dealStage
+  }
 }
