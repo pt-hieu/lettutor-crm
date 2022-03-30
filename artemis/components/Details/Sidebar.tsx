46e502450d614f3c7bd665739e2137b5b2634740
Le Hao <o18ahl2811@gmail.com>
2022-03-31 01:55:21 +0700

    feat: add detail pages

---
+++ artemis/components/Details/Sidebar.tsx
@@ -0,0 +1,147 @@
+import { notification } from 'antd'
+import { useRouter } from 'next/router'
+import React, { useMemo } from 'react'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+
+import { AccountDetailSections } from '@components/Accounts/AccountDetailSidebar'
+
+import AddTaskModal, { TaskFormData } from '@utils/components/AddTaskModal'
+import DetailPageSidebar, {
+  SidebarStructure,
+} from '@utils/components/DetailPageSidebar'
+import { useModal } from '@utils/hooks/useModal'
+import { TaskStatus } from '@utils/models/task'
+import { getAccount } from '@utils/service/account'
+import { addTask } from '@utils/service/task'
+
+export const DetailSidebar = () => {
+  const router = useRouter()
+  const accountId = router.query.id as string
+
+  const client = useQueryClient()
+  const [createTaskVisible, openCreateTask, closeCreateTask] = useModal()
+
+  const { isLoading, mutateAsync } = useMutation('add-task', addTask, {
+    onSuccess: () => {
+      client.refetchQueries(['account', accountId])
+      notification.success({
+        message: 'Add task successfully.',
+      })
+    },
+    onError: () => {
+      notification.error({ message: 'Add task unsuccessfully.' })
+    },
+  })
+
+  const { data: account } = useQuery(
+    ['account', accountId],
+    getAccount(accountId),
+    { enabled: false },
+  )
+
+  const { open, close } = useMemo(
+    () => ({
+      open: account?.tasksToDisplay.filter(
+        (task) => task.status !== TaskStatus.COMPLETED,
+      ).length,
+      close: account?.tasksToDisplay.filter(
+        (task) => task.status === TaskStatus.COMPLETED,
+      ).length,
+    }),
+    [account],
+  )
+
+  const createTask = (formData: TaskFormData) => {
+    if (!formData.dueDate) {
+      formData.dueDate = null
+    }
+
+    mutateAsync({
+      ...formData,
+      leadId: null,
+      contactId: null,
+      accountId,
+      dealId: null,
+    })
+
+    closeCreateTask()
+  }
+
+  const SideBarItems: SidebarStructure = [
+    {
+      title: 'Related List',
+      options: [
+        {
+          label: AccountDetailSections.Notes,
+        },
+        {
+          label: AccountDetailSections.Deals,
+        },
+        {
+          id: AccountDetailSections.OpenActivities,
+          label: (
+            <span>
+              {AccountDetailSections.OpenActivities}
+              {!!open && (
+                <span className="ml-3 bg-blue-600 text-white rounded-md p-1 px-2">
+                  {open}
+                </span>
+              )}
+            </span>
+          ),
+          extend: {
+            title: 'Add A Task',
+            onClick: () => openCreateTask(),
+          },
+        },
+        {
+          id: AccountDetailSections.ClosedActivities,
+          label: (
+            <span>
+              {AccountDetailSections.ClosedActivities}
+              {!!close && (
+                <span className="ml-3 bg-blue-600 text-white rounded-md p-1 px-2">
+                  {close}
+                </span>
+              )}
+            </span>
+          ),
+        },
+        {
+          id: AccountDetailSections.Attachments,
+          label: (
+            <span>
+              {AccountDetailSections.Attachments}
+              {!!account?.attachments.length && (
+                <span className="ml-3 bg-blue-600 text-white rounded-md p-1 px-2">
+                  {account.attachments.length}
+                </span>
+              )}
+            </span>
+          ),
+          extend: {
+            title: 'Add attachments',
+            onClick: () =>
+              client.setQueryData('cmd:add-attachment', Date.now()),
+          },
+        },
+        {
+          label: AccountDetailSections.Logs,
+        },
+      ],
+    },
+  ]
+
+  return (
+    <div className="relative">
+      <AddTaskModal
+        visible={createTaskVisible}
+        handleClose={closeCreateTask}
+        handleCreateTask={createTask}
+        isLoading={isLoading}
+      />
+
+      <DetailPageSidebar data={SideBarItems} />
+    </div>
+  )
+}
--- artemis/components/Module/DetailView.tsx
+++ artemis/components/Module/DetailView.tsx
@@ -1,7 +1,134 @@
-import Layout from "@utils/components/Layout";
+import { capitalize } from 'lodash'
+import { GetServerSideProps } from 'next'
+import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
+import { QueryClient, dehydrate, useQuery } from 'react-query'
 
-export default function DetailView() {
-  return <Layout>
-    <div></div>
-  </Layout>
+import { DetailNavbar } from '@components/Details/Navbar'
+import { DetailSidebar } from '@components/Details/Sidebar'
+
+import InlineEdit from '@utils/components/InlineEdit'
+import { Props } from '@utils/components/Input'
+import Layout from '@utils/components/Layout'
+import { getSessionToken } from '@utils/libs/getToken'
+import { getEntity } from '@utils/service/module'
+import { getRawUsers } from '@utils/service/user'
+
+import { toCapitalizedWords } from './OverviewView'
+
+type TProps = {
+  paths: string[]
+}
+
+type EntityInfo = {
+  label: string
+  props: Omit<Props<'input' | 'textarea' | 'select' | undefined>, 'editable'>
+}
+
+export default function DetailView({ paths }: TProps) {
+  const [moduleName, id] = paths
+  const { data: entity } = useQuery([moduleName, id], getEntity(moduleName, id))
+  const { data: users } = useQuery('users', getRawUsers())
+
+  const entityData = entity?.data || {}
+  const metaData = entity?.module.meta || []
+
+  const {
+    register,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+  })
+
+  const entityInfo: EntityInfo[] = metaData.map(
+    ({ name, options, required, type }) => ({
+      label: toCapitalizedWords(name.replace('Id', '')),
+      props: {
+        as: type === 'Select' || type === 'Relation' ? 'select' : 'input',
+        error: errors[name]?.message,
+        props: {
+          //TODO: need to refactor in the future
+          children: options ? (
+            <>
+              {options?.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </>
+          ) : name === 'ownerId' ? (
+            <>
+              {users?.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </>
+          ) : undefined,
+          ...register(name, { required }),
+          id: name,
+        },
+      },
+    }),
+  )
+
+  useEffect(() => {
+    reset(entityData)
+  }, [entityData])
+
+  return (
+    <Layout title={`CRM | ${capitalize(moduleName)} | ${entity?.name}`}>
+      <div className="crm-container">
+        <DetailNavbar data={entity} />
+
+        <div className="grid grid-cols-[250px,1fr]">
+          <DetailSidebar />
+
+          <div className="flex flex-col gap-4 ml-5">
+            <div>
+              <div className="font-semibold mb-4 text-[17px]">Overview</div>
+              <form onSubmit={() => {}} className="flex flex-col gap-4">
+                {entityInfo.map(({ label, props }) => (
+                  <div key={label} className="grid grid-cols-[250px,1fr] gap-4">
+                    <span className="inline-block text-right font-medium pt-[10px]">
+                      {label}
+                    </span>
+
+                    <InlineEdit
+                      onEditCancel={() => {}}
+                      onEditComplete={() => {}}
+                      {...props}
+                    />
+                  </div>
+                ))}
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  query,
+}) => {
+  const client = new QueryClient()
+  const token = getSessionToken(req.cookies)
+  const paths = query.path
+  const [moduleName, id] = paths || []
+
+  await Promise.all([
+    client.prefetchQuery([moduleName, id], getEntity(moduleName, id, token)),
+    client.prefetchQuery('users', getRawUsers(token)),
+  ])
+
+  return {
+    props: {
+      dehydratedState: dehydrate(client),
+      paths,
+    },
+  }
 }
