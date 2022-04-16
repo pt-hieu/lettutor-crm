c3e7b2d7060ed1c67e3f5c322a3db8d9f0826902
Pham Viet Hoa <hoapham@kms-technology.com>
2022-04-16 18:56:46 +0700

    feat: update update role api

--- apollo/src/role/role.service.ts
+++ apollo/src/role/role.service.ts
@@ -90,10 +90,15 @@ export class RoleService {
     )
       throw new BadRequestException('Name has been taken')
 
+    const actions = await this.actionRepo.find({
+      where: { id: In(dto.actionsId) },
+    })
+
     return this.roleRepo
       .save({
         ...role,
         ...dto,
+        actions: actions,
       })
       .then((res) => {
         this.eventEmitter.emit('auth.invalidate', id)
