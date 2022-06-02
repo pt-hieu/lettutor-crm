9fd62b9713293005e9a4fc414ae8315f8390cea4
hieulm2k <leminhhhieust@gmail.com>
2022-06-02 22:17:13 +0700

    feat: deals and tasks feed management

--- apollo/src/feed/comment.entity.ts
+++ apollo/src/feed/comment.entity.ts
@@ -2,18 +2,32 @@ import { Exclude } from 'class-transformer'
 import { Column, Entity, ManyToOne, OneToMany } from 'typeorm'
 
 import { File } from 'src/file/file.entity'
+import { Log } from 'src/log/log.entity'
 import { Ownerful } from 'src/utils/owner.entity'
 
 import { Status } from './status.entity'
 
 @Entity({ name: 'comment' })
 export class Comment extends Ownerful {
-  @ManyToOne(() => Status, (status) => status.comments, { onDelete: 'CASCADE' })
+  @ManyToOne(() => Status, (status) => status.comments, {
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
   status: Status
 
   @Column({ type: 'uuid' })
   @Exclude({ toPlainOnly: true })
-  statusId: string
+  statusId: string | null
+
+  @ManyToOne(() => Log, (log) => log.comments, {
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
+  log: Log
+
+  @Column({ type: 'uuid' })
+  @Exclude({ toPlainOnly: true })
+  logId: string | null
 
   @Column({ type: 'varchar' })
   content: string
