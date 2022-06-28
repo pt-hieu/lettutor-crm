603814eb4e13496da8d379af81d5e19c5c4c1c24
hieulm2k <leminhhhieust@gmail.com>
2022-06-28 22:41:18 +0700

    fix: default deal stage should be more business centrics

--- apollo/src/deal-stage/default.deal-stage.ts
+++ apollo/src/deal-stage/default.deal-stage.ts
@@ -5,21 +5,57 @@ export const defaultDealStages: Pick<
   'name' | 'type' | 'probability' | 'order'
 >[] = [
   {
-    name: 'Deal stage 1',
+    name: 'Qualification',
     type: DealStageType.OPEN,
-    probability: 40,
+    probability: 10,
     order: 1,
   },
   {
-    name: 'Deal stage 2',
-    type: DealStageType.CLOSE_WON,
-    probability: 100,
+    name: 'Need Analysis',
+    type: DealStageType.OPEN,
+    probability: 45,
     order: 2,
   },
   {
-    name: 'Deal stage 3',
+    name: 'Value Proposition',
+    type: DealStageType.OPEN,
+    probability: 40,
+    order: 3,
+  },
+  {
+    name: 'Identify Decision Makers',
+    type: DealStageType.OPEN,
+    probability: 60,
+    order: 4,
+  },
+  {
+    name: 'Proposal / Price Quote',
+    type: DealStageType.OPEN,
+    probability: 75,
+    order: 5,
+  },
+  {
+    name: 'Negotiation / Price Review',
+    type: DealStageType.OPEN,
+    probability: 90,
+    order: 6,
+  },
+  {
+    name: 'Closed Won',
+    type: DealStageType.CLOSE_WON,
+    probability: 100,
+    order: 7,
+  },
+  {
+    name: 'Closed Lost',
     type: DealStageType.CLOSE_LOST,
     probability: 0,
-    order: 3,
+    order: 8,
+  },
+  {
+    name: 'Closed Lost to Competition',
+    type: DealStageType.CLOSE_LOST,
+    probability: 0,
+    order: 9,
   },
 ]
